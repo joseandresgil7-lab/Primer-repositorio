2f2e3b0cb68eb8134570988ab134e1601e295237
joseandresgil7-lab <joseandresgil7@gmail.com>
2026-08-13 20:35:56 -0400

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,1 +1,18 @@
 print("mi primer repositorio en GitHub")
+
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "ROFL" : "una respuesta a una broma",
+            "SHEESH" : "ligera desaprobación",
+            "CREEPY" : "aterrador, siniestro",
+            "AGGRO" : "ponerse agresivo/enojado",
+            }
+
+for i in range(5):
+    word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+    
+    if word in meme_dict.keys():
+        print(word, "significa:",meme_dict[word])
+    else:
+        print(word, "no esta en el diccionario... ")
